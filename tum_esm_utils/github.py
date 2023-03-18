9bde7096b477644c121dad3e85494de5b351b1fa
dostuffthatmatters <moritz@dostuffthatmatters.dev>
2023-03-18 13:12:58 +0100

    Add `request_github_file`

---
+++ tum_esm_utils/github.py
@@ -0,0 +1,20 @@
+from typing import Any, Optional
+import requests
+
+
+def request_github_file(
+    github_repository: str,
+    filepath: str,
+    access_token: Optional[str] = None,
+) -> list[Any]:
+    """Sends a request and returns the content of the response, in unicode."""
+    response = requests.get(
+        f"https://raw.githubusercontent.com/{github_repository}/main/{filepath}",
+        headers={
+            "Authorization": f"token {access_token}",
+            "Accept": "application/text",
+        },
+        timeout=10,
+    )
+    response.raise_for_status()
+    return response.text
