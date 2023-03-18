bda3efa23a82bbb362b7a273874bd57086f7fee3
dostuffthatmatters <moritz@dostuffthatmatters.dev>
2023-03-18 11:54:19 +0100

    Add shell utils

---
+++ tum_esm_utils/__init__.py
@@ -0,0 +1,1 @@
+from .shell_commands import run_shell_command, CommandLineException, get_hostname
---
+++ tum_esm_utils/shell_commands.py
@@ -0,0 +1,52 @@
+import os
+import subprocess
+from typing import Optional
+
+
+class CommandLineException(Exception):
+    def __init__(self, value: str, details: Optional[str] = None) -> None:
+        self.value = value
+        self.details = details
+        Exception.__init__(self)
+
+    def __str__(self) -> str:
+        return repr(self.value)
+
+
+def run_shell_command(
+    command: str,
+    working_directory: Optional[str] = None,
+    executable: str = "/bin/bash",
+) -> str:
+    """runs a shell command and raises a `CommandLineException`
+    if the return code is not zero, returns the stdout. Uses
+    `/bin/bash` by default."""
+
+    p = subprocess.run(
+        command,
+        shell=True,
+        stdout=subprocess.PIPE,
+        stderr=subprocess.PIPE,
+        cwd=working_directory,
+        env=os.environ.copy(),
+        executable=executable,
+    )
+    stdout = p.stdout.decode("utf-8", errors="replace").strip()
+    stderr = p.stderr.decode("utf-8", errors="replace").strip()
+
+    if p.returncode != 0:
+        raise CommandLineException(
+            f"command '{command}' failed with exit code {p.returncode}",
+            details=f"\nstderr:\n{stderr}\nstout:\n{stdout}",
+        )
+
+    return stdout
+
+
+def get_hostname() -> str:
+    """returns the hostname of the device, removes network
+    postfix (`somename.local`) if present. Only works reliably,
+    when the hostname doesn't contain a dot."""
+
+    raw = run_shell_command("hostname")
+    return (raw.split(".")[0]) if ("." in raw) else raw
