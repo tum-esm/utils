e81be85c38bcf2a15782ce55568a5741dcf5ec7f
dostuffthatmatters <moritz@dostuffthatmatters.dev>
2023-03-18 13:03:16 +0100

    Add more string functions

--- tum_esm_utils/string.py
+++ tum_esm_utils/string.py
@@ -1,5 +1,7 @@
+from datetime import datetime
 import random
 import string
+from typing import Literal
 
 
 def get_random_string(length: int, forbidden: list[str] = []) -> str:
@@ -11,3 +13,34 @@ def get_random_string(length: int, forbidden: list[str] = []) -> str:
         if output not in forbidden:
             break
     return output
+
+
+def pad_string(
+    text: str,
+    min_width: int,
+    pad_position: Literal["left", "right"] = "left",
+    fill_char: Literal["0", " ", "-", "_"] = " ",
+) -> str:
+    if len(text) >= min_width:
+        return text
+    else:
+        pad = fill_char * (min_width - len(text))
+        return (pad + text) if (pad_position == "left") else (text + pad)
+
+
+def is_date_string(date_string: str) -> bool:
+    """Returns true if string is in `YYYYMMDD` format and date exists"""
+    try:
+        datetime.strptime(date_string, "%Y%m%d")
+        return True
+    except (AssertionError, ValueError):
+        return False
+
+
+def insert_replacements(content: str, replacements: dict[str, str]) -> str:
+    """For every key in replacements, replaces `%key$` in the
+    content with its value."""
+
+    for key, value in replacements.items():
+        content = content.replace(f"%{key}%", value)
+    return content
