0ad6f89b2c6f6dbabc7e9de31caef70c97a32dbe
dostuffthatmatters <moritz@dostuffthatmatters.dev>
2023-03-18 13:48:31 +0100

    Debug CI

--- tests/test_with_filelock.py
+++ tests/test_with_filelock.py
@@ -6,7 +6,7 @@ import multiprocessing
 from typing import Optional
 from tum_esm_utils.decorators import with_filelock
 
-
+TIMEOUT_UNIT = 3
 res_queue_th: queue.Queue[int] = queue.Queue()
 res_queue_mp: queue.Queue[int] = multiprocessing.Queue()
 lockfile_path = os.path.join(
@@ -25,14 +25,14 @@ def count_queue_items(q: queue.Queue[int]) -> int:
             return c
 
 
-@with_filelock(lockfile_path=lockfile_path, timeout=1.5)
+@with_filelock(lockfile_path=lockfile_path, timeout=TIMEOUT_UNIT * 2)
 def f(delay: int = 0, q: Optional[queue.Queue[int]] = None) -> int:
     """this funtion will sleep for the amount passed as `delay`, put
     1 into the passed queue (optional) and return 1. It will raise a
     timeout exception when the filelock-aquiring takes longer than
     one second."""
 
-    time.sleep(delay)
+    time.sleep(TIMEOUT_UNIT * delay)
     if q is not None:
         q.put(1)
     return 1
@@ -46,52 +46,36 @@ def test_filelock_without_concurrency() -> None:
 
 
 def test_filelock_with_threading() -> None:
-    t1 = threading.Thread(target=f, kwargs={"delay": 0.75, "q": res_queue_th})
-    t2 = threading.Thread(target=f, kwargs={"delay": 0.75, "q": res_queue_th})
+    t1 = threading.Thread(target=f, kwargs={"delay": 1, "q": res_queue_th})
+    t2 = threading.Thread(target=f, kwargs={"delay": 1, "q": res_queue_th})
     t1.start()
     t2.start()
     t1.join()
     t2.join()
     assert count_queue_items(res_queue_th) == 2
 
-    t3 = threading.Thread(target=f, kwargs={"delay": 2.25, "q": res_queue_th})
-    t4 = threading.Thread(target=f, kwargs={"delay": 2.25, "q": res_queue_th})
+    t3 = threading.Thread(target=f, kwargs={"delay": 3, "q": res_queue_th})
+    t4 = threading.Thread(target=f, kwargs={"delay": 3, "q": res_queue_th})
     t3.start()
     t4.start()
     t3.join()
     t4.join()
     assert count_queue_items(res_queue_th) == 1
 
-    t5 = threading.Thread(target=f, kwargs={"delay": 0.75, "q": res_queue_th})
-    t6 = threading.Thread(target=f, kwargs={"delay": 0.75, "q": res_queue_th})
-    t5.start()
-    t6.start()
-    t5.join()
-    t6.join()
-    assert count_queue_items(res_queue_th) == 2
-
 
 def test_filelock_with_multiprocessing() -> None:
-    t1 = threading.Thread(target=f, kwargs={"delay": 0.75, "q": res_queue_mp})
-    t2 = threading.Thread(target=f, kwargs={"delay": 0.75, "q": res_queue_mp})
+    t1 = threading.Thread(target=f, kwargs={"delay": 1, "q": res_queue_mp})
+    t2 = threading.Thread(target=f, kwargs={"delay": 1, "q": res_queue_mp})
     t1.start()
     t2.start()
     t1.join()
     t2.join()
     assert count_queue_items(res_queue_mp) == 2
 
-    t3 = threading.Thread(target=f, kwargs={"delay": 2.25, "q": res_queue_mp})
-    t4 = threading.Thread(target=f, kwargs={"delay": 2.25, "q": res_queue_mp})
+    t3 = threading.Thread(target=f, kwargs={"delay": 3, "q": res_queue_mp})
+    t4 = threading.Thread(target=f, kwargs={"delay": 3, "q": res_queue_mp})
     t3.start()
     t4.start()
     t3.join()
     t4.join()
     assert count_queue_items(res_queue_mp) == 1
-
-    t5 = threading.Thread(target=f, kwargs={"delay": 0.75, "q": res_queue_mp})
-    t6 = threading.Thread(target=f, kwargs={"delay": 0.75, "q": res_queue_mp})
-    t5.start()
-    t6.start()
-    t5.join()
-    t6.join()
-    assert count_queue_items(res_queue_mp) == 2
