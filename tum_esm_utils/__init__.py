c8078e44e49bf3d64dc48b87ef914286d568724d
dostuffthatmatters <moritz@dostuffthatmatters.dev>
2023-05-08 22:47:10 +0200

    Improve top level doc string

--- tum_esm_utils/__init__.py
+++ tum_esm_utils/__init__.py
@@ -1,5 +1,9 @@
 """Python utilities by the Professorship of Environmental
-Sensing and Modeling at the Technical University of Munich"""
+Sensing and Modeling at the Technical University of Munich.
+
+GitHub Repository https://github.com/tum-esm/utils
+Documentation: https://tum-esm.github.io/utils/
+PyPI: https://pypi.org/project/tum-esm-utils/"""
 
 from . import (
     context,
