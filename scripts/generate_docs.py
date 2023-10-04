ebaa180f6c53377ba0ac3002619c055195a49ed3
Moritz Makowski <moritz@dostuffthatmatters.dev>
2023-10-04 18:48:32 +0200

    Move from `lazydocs` to  `pydoc-markdown`

---
+++ scripts/generate_docs.py
@@ -0,0 +1,57 @@
+import os
+import tempfile
+
+PROJECT_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+INDEX_SRC = os.path.join(PROJECT_DIR, "README.md")
+INDEX_DST = os.path.join(PROJECT_DIR, "docs", "pages", "index.md")
+API_DST = os.path.join(PROJECT_DIR, "docs", "pages", "api-reference.md")
+
+# copy index file to docs folder
+
+with open(INDEX_SRC, "r") as f:
+    index_file_content = f.read()
+
+with open(INDEX_DST, "w") as f:
+    f.write(index_file_content)
+
+# generate automatic API reference and prettify output
+
+with tempfile.NamedTemporaryFile() as f:
+    os.system(
+        f"cd {PROJECT_DIR} && pydoc-markdown --package=tum_esm_utils > {f.name}"
+    )
+    with open(f.name, "r") as f2:
+        raw_api_reference_content = f2.read()
+
+    parsed_api_reference_content_lines: list[str] = []
+    for line in raw_api_reference_content.split("\n"):
+        if line.startswith('<a id="'):
+            continue
+        if line.startswith("#"):
+            line_segments = line.split(" ")
+            assert line_segments[0] == "#" * len(line_segments[0])
+            if len(line_segments) == 2:
+                parsed_api_reference_content_lines.append(
+                    line_segments[0] + " `" +
+                    line_segments[1].replace("\\_", "_") + "`"
+                )
+            elif len(line_segments) == 3:
+                assert line_segments[2] == "Objects"
+                parsed_api_reference_content_lines.append(
+                    line_segments[0] + " `" +
+                    line_segments[1].replace("\\_", "_") + "` Objects"
+                )
+            else:
+                raise ValueError("Unexpected line format: " + line)
+        else:
+            parsed_api_reference_content_lines.append(line)
+
+    parsed_api_reference_content = "\n".join(
+        parsed_api_reference_content_lines[1 :]
+    )
+
+with open(API_DST, "w") as f:
+    f.write(
+        "# API Reference of the `tum_esm_utils` package\n\n" +
+        parsed_api_reference_content
+    )
--- scripts/update_api_reference.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-set -o errexit
-
-rm docs/README.md
-cp README.md docs/README.md
-
-lazydocs --output-path ./docs/api-reference/ --src-base-url https://github.com/tum-esm/utils/tree/main/ --no-watermark tum_esm_utils
