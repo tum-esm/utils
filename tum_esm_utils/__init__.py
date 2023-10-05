23db3decfacd27817c6cc558f88a02bcbb2880ff
Moritz Makowski <moritz@dostuffthatmatters.dev>
2023-10-05 12:39:46 +0200

    Fix link to documentation

--- tum_esm_utils/__init__.py
+++ tum_esm_utils/__init__.py
@@ -2,8 +2,8 @@
 Sensing and Modeling at the Technical University of Munich.
 
 GitHub Repository https://github.com/tum-esm/utils
-Documentation: https://tum-esm-utils.netlify.app/
-PyPI: https://pypi.org/project/tum-esm-utils/"""
+Documentation: https://tum-esm-utils.netlify.app
+PyPI: https://pypi.org/project/tum-esm-utils"""
 
 from . import (
     context,
