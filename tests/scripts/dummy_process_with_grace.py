5315c685901eafb8f33fd424b7cf8407df815a57
Moritz Makowski <moritz@dostuffthatmatters.dev>
2023-11-13 15:47:41 +0100

    #10 Improve tests on background processes

---
+++ tests/scripts/dummy_process_with_grace.py
@@ -0,0 +1,24 @@
+from typing import Any
+import time
+import signal
+
+print("starting")
+
+
+def _graceful_teardown(*args: Any) -> None:
+    print("starting graceful teardown")
+    time.sleep(3)
+    print("finished graceful teardown")
+    exit(0)
+
+
+print("registering teardown handlers")
+
+signal.signal(signal.SIGINT, _graceful_teardown)
+signal.signal(signal.SIGTERM, _graceful_teardown)
+
+print("waiting")
+
+time.sleep(3)
+
+print("stopping")
--- tests/test_processes.py
+++ tests/test_processes.py
@@ -1,36 +1,46 @@
-from typing import Any
 import os
 import sys
 import time
-import pytest
 import tum_esm_utils
+from tum_esm_utils.processes import get_process_pids, start_background_process, terminate_process
 
-SCRIPT_PATH = os.path.join(
-    os.path.dirname(os.path.dirname(os.path.abspath(__file__))),
-    "scripts",
-    "dummy_process_script.py",
+PROJECT_DIR = tum_esm_utils.files.get_parent_dir_path(__file__, current_depth=2)
+
+SCRIPT_PATH = os.path.join(PROJECT_DIR, "tests", "scripts", "dummy_process.py")
+SCRIPT_PATH_WITH_GRACE = os.path.join(
+    PROJECT_DIR, "tests", "scripts", "dummy_process_with_grace.py"
 )
 
 
-@pytest.fixture
-def _dummy_process_script() -> Any:
-    with open(SCRIPT_PATH, "w") as f:
-        f.write("import time\ntime.sleep(30)")
-    yield
-    os.remove(SCRIPT_PATH)
+def test_ungraceful_process() -> None:
+    expected_pid = start_background_process(sys.executable, SCRIPT_PATH)
+    time.sleep(0.1)
+
+    assert get_process_pids(SCRIPT_PATH) == [expected_pid]
+    time.sleep(0.1)
+
+    assert terminate_process(SCRIPT_PATH) == [expected_pid]
+    time.sleep(0.1)
+
+    assert get_process_pids(SCRIPT_PATH) == []
 
 
-def test_processes(_dummy_process_script: Any) -> None:
-    expected_pid = tum_esm_utils.processes.start_background_process(
-        sys.executable, SCRIPT_PATH
+def test_graceful_process() -> None:
+    expected_pid = start_background_process(
+        sys.executable, SCRIPT_PATH_WITH_GRACE
     )
+    time.sleep(0.1)
 
-    time.sleep(1)
+    assert get_process_pids(SCRIPT_PATH_WITH_GRACE) == [expected_pid]
+    time.sleep(0.1)
 
-    assert tum_esm_utils.processes.get_process_pids(SCRIPT_PATH) == [
-        expected_pid
-    ]
-    assert tum_esm_utils.processes.terminate_process(SCRIPT_PATH) == [
-        expected_pid
-    ]
-    assert tum_esm_utils.processes.get_process_pids(SCRIPT_PATH) == []
+    # cannot terminate because teardown takes long
+    assert terminate_process(SCRIPT_PATH_WITH_GRACE) == [expected_pid]
+    time.sleep(0.5)
+    assert get_process_pids(SCRIPT_PATH_WITH_GRACE) == [expected_pid]
+
+    # hard kill works
+    assert terminate_process(SCRIPT_PATH_WITH_GRACE,
+                             termination_timeout=0) == [expected_pid]
+    time.sleep(0.1)
+    assert get_process_pids(SCRIPT_PATH_WITH_GRACE) == []
