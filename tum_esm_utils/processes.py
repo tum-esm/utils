a386eb6600725d4963fab90e43fadc2aca351aeb
Moritz Makowski <moritz@dostuffthatmatters.dev>
2023-11-13 15:47:20 +0100

    #10 Refactor process termination

--- tum_esm_utils/processes.py
+++ tum_esm_utils/processes.py
@@ -6,6 +6,7 @@ Implements: `get_process_pids`, `start_background_process`,
 from __future__ import annotations
 import os
 import time
+from typing import Optional
 import psutil
 
 
@@ -47,16 +48,24 @@ def start_background_process(interpreter_path: str, script_path: str) -> int:
     return new_pids[0]
 
 
-def terminate_process(script_path: str) -> list[int]:
+def terminate_process(
+    script_path: str,
+    termination_timeout: Optional[int] = None,
+) -> list[int]:
     """Terminate all processes that have the given script as their
-    entrypoint. Returns the list of terminated PIDs."""
+    entrypoint. Returns the list of terminated PIDs.
+    
+    If `termination_timeout` is not None, the processes will be
+    terminated forcefully after the given timeout (in seconds)."""
 
-    termination_pids: list[int] = []
+    processes_to_terminate: list[psutil.Process] = []
+
+    # terminate the processes gracefully
     for p in psutil.process_iter():
         try:
             if p.cmdline()[1] == script_path:
-                termination_pids.append(p.pid)
-                p.kill()
+                processes_to_terminate.append(p)
+                p.terminate()
         except (
             psutil.AccessDenied,
             psutil.ZombieProcess,
@@ -65,4 +74,26 @@ def terminate_process(script_path: str) -> list[int]:
         ):
             pass
 
-    return termination_pids
+    # kill the processes using SIGKILL after a timeout
+    if termination_timeout is not None:
+        t1 = time.time()
+        while True:
+            try:
+                if (time.time() - t1) > termination_timeout:
+                    for p in processes_to_terminate:
+                        if p.is_running():
+                            p.kill()
+                if any([p.is_running() for p in processes_to_terminate]):
+                    time.sleep(1)
+                else:
+                    # all processes have gracefully terminated
+                    break
+            except (
+                psutil.AccessDenied,
+                psutil.ZombieProcess,
+                psutil.NoSuchProcess,
+                IndexError,
+            ):
+                pass
+
+    return [p.pid for p in processes_to_terminate]
