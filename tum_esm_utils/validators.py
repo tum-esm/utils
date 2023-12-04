ee2eef0d43d2e0b156874aeb31f1e36bed01283f
Moritz Makowski <moritz@dostuffthatmatters.dev>
2023-12-04 01:01:39 +0100

    Add more docstrings to validators

--- tum_esm_utils/validators.py
+++ tum_esm_utils/validators.py
@@ -1,7 +1,8 @@
 """Implements validator functions for use with pydantic models.
 
 Implements: `validate_bool`, `validate_float`, `validate_int`,
-`validate_str`, `validate_list`"""
+`validate_str`, `validate_list`, `StrictFilePath`,
+`StrictDirectoryPath`."""
 
 from __future__ import annotations
 from typing import Any, Callable, Optional, TypeVar
@@ -206,6 +207,27 @@ def validate_list(
 
 
 class StrictFilePath(pydantic.RootModel[str]):
+    """A pydantic model that validates a file path.
+    
+    Example usage:
+    
+    ```python
+    class MyModel(pyndatic.BaseModel):
+        path: StrictFilePath
+
+    m = MyModel(path='/path/to/file') # validates that the file exists
+    ```
+
+    The validation can be ignored by setting the context variable:
+
+    ```python
+    m = MyModel.model_validate(
+        {"path": "somenonexistingpath"},
+        context={"ignore-path-existence": True},
+    ) # does not raise an error
+    ```
+    """
+
     root: str
 
     @pydantic.field_validator('root')
@@ -220,6 +242,27 @@ class StrictFilePath(pydantic.RootModel[str]):
 
 
 class StrictDirectoryPath(pydantic.RootModel[str]):
+    """A pydantic model that validates a directory path.
+    
+    Example usage:
+    
+    ```python
+    class MyModel(pyndatic.BaseModel):
+        path: StrictDirectoryPath
+
+    m = MyModel(path='/path/to/directory') # validates that the directory exists
+    ```
+
+    The validation can be ignored by setting the context variable:
+
+    ```python
+    m = MyModel.model_validate(
+        {"path": "somenonexistingpath"},
+        context={"ignore-path-existence": True},
+    ) # does not raise an error
+    ```
+    """
+
     root: str
 
     @pydantic.field_validator('root')
