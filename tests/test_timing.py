3c0bf46f86c3a4852e29697a9ace185ac586a806
Moritz Makowski <moritz@dostuffthatmatters.dev>
2023-12-29 11:30:10 +0100

    Add test for `timing.date_range`

--- tests/test_timing.py
+++ tests/test_timing.py
@@ -1,6 +1,7 @@
 from __future__ import annotations
 import time
 import tum_esm_utils
+import datetime
 
 DURATION = 0.75
 
@@ -36,3 +37,49 @@ def test_clear_alarm() -> None:
     tum_esm_utils.timing.set_alarm(1, "test")
     tum_esm_utils.timing.clear_alarm()
     func()
+
+
+# write a unit test for the function `date_range` in `tum_esm_utils/text.py`
+# write a bunch of sample inputs and expected outputs
+def test_date_range() -> None:
+    assert tum_esm_utils.timing.date_range(
+        datetime.date(2021, 1, 1), datetime.date(2021, 1, 1)
+    ) == [datetime.date(2021, 1, 1)]
+    assert tum_esm_utils.timing.date_range(
+        datetime.date(2021, 1, 1), datetime.date(2021, 1, 2)
+    ) == [
+        datetime.date(2021, 1, 1),
+        datetime.date(2021, 1, 2),
+    ]
+    assert tum_esm_utils.timing.date_range(
+        datetime.date(2021, 1, 1), datetime.date(2021, 1, 3)
+    ) == [
+        datetime.date(2021, 1, 1),
+        datetime.date(2021, 1, 2),
+        datetime.date(2021, 1, 3),
+    ]
+
+    # test for inputs that are in different months
+    assert tum_esm_utils.timing.date_range(
+        datetime.date(2021, 1, 28), datetime.date(2021, 2, 2)
+    ) == [
+        datetime.date(2021, 1, 28),
+        datetime.date(2021, 1, 29),
+        datetime.date(2021, 1, 30),
+        datetime.date(2021, 1, 31),
+        datetime.date(2021, 2, 1),
+        datetime.date(2021, 2, 2),
+    ]
+
+    # test for inputs that are in different years
+    assert tum_esm_utils.timing.date_range(
+        datetime.date(2021, 12, 28), datetime.date(2022, 1, 3)
+    ) == [
+        datetime.date(2021, 12, 28),
+        datetime.date(2021, 12, 29),
+        datetime.date(2021, 12, 30),
+        datetime.date(2021, 12, 31),
+        datetime.date(2022, 1, 1),
+        datetime.date(2022, 1, 2),
+        datetime.date(2022, 1, 3),
+    ]
