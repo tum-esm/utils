ba0a406404e6f9438718bc325fde48bf5ebfc8ef
Moritz Makowski <moritz@dostuffthatmatters.dev>
2024-01-29 14:53:14 +0100

    Fix bug in ifg parser bundle
    Until now, the bundle included the compiled files. I hope that this fixes the bug we had on other systems.

--- tests/test_interferograms.py
+++ tests/test_interferograms.py
@@ -10,6 +10,7 @@ _PROJECT_DIR = tum_esm_utils.files.get_parent_dir_path(
 def test_detect_corrupt_ifgs() -> None:
     """create a temporary directory with a file that
     is not an interferogram at all"""
+
     with tempfile.TemporaryDirectory() as tmpdir:
         with open(os.path.join(tmpdir, "test_ifg"), "w") as f:
             f.write("corrupt interferogram")
