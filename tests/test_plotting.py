5a0aff52039a08955e721a6c35c417ba27e8b031
Moritz Makowski <moritz@dostuffthatmatters.dev>
2024-04-14 15:59:11 +0200

    Debug pytests

--- tests/test_plotting.py
+++ tests/test_plotting.py
@@ -8,19 +8,19 @@ def test_plotting() -> None:
     if os.path.exists(figure_path):
         os.remove(figure_path)
 
-    tum_esm_utils.optional.plotting.apply_better_defaults(
+    tum_esm_utils.plotting.apply_better_defaults(
         font_family="Roboto" if platform.system() == "Darwin" else None
     )
 
-    with tum_esm_utils.optional.plotting.create_figure(
+    with tum_esm_utils.plotting.create_figure(
         "/tmp/tum_esm_utils_plotting_test.png"
     ) as fig:
-        axis1 = tum_esm_utils.optional.plotting.add_subplot(
+        axis1 = tum_esm_utils.plotting.add_subplot(
             fig, (2, 1, 1), title="Test Plot", xlabel="X", ylabel="Y"
         )
         axis1.plot([1, 2, 3], [1, 2, 3])
 
-        axis2 = tum_esm_utils.optional.plotting.add_subplot(
+        axis2 = tum_esm_utils.plotting.add_subplot(
             fig, (2, 1, 2), title="Test Plot 2", xlabel="X", ylabel="Y"
         )
         axis2.plot([1, 2, 3], [3, 2, 1])
