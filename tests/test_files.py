6d1ab245b39f3e3a43fa80e01d4e019f90eae216
Moritz Makowski <moritz@dostuffthatmatters.dev>
2024-04-14 12:30:51 +0200

    Remove invalid tests

--- tests/test_files.py
+++ tests/test_files.py
@@ -18,45 +18,6 @@ def test_get_parent_dir_path() -> None:
     assert parent_parent_dir == os.path.dirname(parent_dir)
 
 
-# test load_raw_proffast_output by generating a valid temporary file
-# and trying to load it with all columns and a subset of columns
-# use the polars library
-def test_load_raw_proffast_output() -> None:
-    data_column_names = {
-        "gnd_p": " gndP",
-        "gnd_t": " gndT",
-        "app_sza": " appSZA",
-        "azimuth": " azimuth",
-        "xh2o": " XH2O",
-        "xair": " XAIR",
-        "xco2": " XCO2",
-        "xch4": " XCH4",
-        "xco": " XCO",
-        "xch4_s5p": " XCH4_S5P",
-    }
-
-    path = os.path.join(
-        PROJECT_DIR,
-        "tests",
-        "data",
-        "comb_invparms_ma_SN061_210329-210329.csv",
-    )
-
-    # load the temporary file
-    df = tum_esm_utils.files.load_raw_proffast_output(path)
-    assert isinstance(df, pl.DataFrame)
-    assert df.shape == (5, 11)  # 5 rows, 11 columns
-    assert df.columns == ["utc"] + list(data_column_names.keys())
-
-    # load the temporary file with a subset of columns
-    df = tum_esm_utils.files.load_raw_proffast_output(
-        path, selected_columns=["gnd_p", "gnd_t", "xair", "xch4_s5p"]
-    )
-    assert isinstance(df, pl.DataFrame)
-    assert df.shape == (5, 5)  # 5 rows, 5 columns
-    assert df.columns == ["utc", "gnd_p", "gnd_t", "xair", "xch4_s5p"]
-
-
 def test_rel_to_abs_path() -> None:
     a1 = tum_esm_utils.files.rel_to_abs_path("tests/data/some.csv")
     a2 = tum_esm_utils.files.rel_to_abs_path("tests", "data", "some.csv")
--- tests/test_text.py
+++ tests/test_text.py
@@ -1,4 +1,3 @@
-import pytest
 import tum_esm_utils
 
 
@@ -23,18 +22,6 @@ def test_is_date_string() -> None:
     assert tum_esm_utils.text.is_date_string("20230101")
     assert not tum_esm_utils.text.is_date_string("20231301")
     assert not tum_esm_utils.text.is_date_string("20230132")
-
-
-@pytest.mark.filterwarnings("ignore::DeprecationWarning")
-def test_is_datetime_string() -> None:
-    assert tum_esm_utils.text.is_datetime_string("20230101 00:00:00")
-    assert not tum_esm_utils.text.is_datetime_string("20231301 00:00:00")
-    assert not tum_esm_utils.text.is_datetime_string("20230132 00:00:00")
-
-    assert tum_esm_utils.text.is_datetime_string("20230101 13:47:59")
-    assert not tum_esm_utils.text.is_datetime_string("20230101 24:47:59")
-    assert not tum_esm_utils.text.is_datetime_string("20230101 13:60:59")
-    assert not tum_esm_utils.text.is_datetime_string("20230101 13:47:65")
 
 
 def test_is_rfc3339_datetime_string() -> None:
@@ -64,39 +51,3 @@ def test_insert_replacements() -> None:
             "Hello %YOU%!", {"YOU": "replacement"}
         ) == "Hello replacement!"
     )
-
-
-# write a unit test for the function `date_range` in `tum_esm_utils/text.py`
-# write a bunch of sample inputs and expected outputs
-def test_date_range() -> None:
-    assert tum_esm_utils.text.date_range("20210101", "20210101") == ["20210101"]
-    assert tum_esm_utils.text.date_range("20210101", "20210102") == [
-        "20210101",
-        "20210102",
-    ]
-    assert tum_esm_utils.text.date_range("20210101", "20210103") == [
-        "20210101",
-        "20210102",
-        "20210103",
-    ]
-
-    # test for inputs that are in different months
-    assert tum_esm_utils.text.date_range("20210128", "20210202") == [
-        "20210128",
-        "20210129",
-        "20210130",
-        "20210131",
-        "20210201",
-        "20210202",
-    ]
-
-    # test for inputs that are in different years
-    assert tum_esm_utils.text.date_range("20211228", "20220103") == [
-        "20211228",
-        "20211229",
-        "20211230",
-        "20211231",
-        "20220101",
-        "20220102",
-        "20220103",
-    ]
