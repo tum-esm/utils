f0bb840bb2ce0a81a3a8fa122185e2c8f2c66e23
Moritz Makowski <moritz.makowski@tum.de>
2024-08-07 18:05:19 +0200

    #19 Add `Version` validator (1)
    implement

--- tum_esm_utils/validators.py
+++ tum_esm_utils/validators.py
@@ -3,6 +3,7 @@
 Implements: `StrictFilePath`, `StrictDirectoryPath`"""
 
 from __future__ import annotations
+from typing import Literal, Optional
 import os
 import pydantic
 
@@ -75,3 +76,79 @@ class StrictDirectoryPath(pydantic.RootModel[str]):
         if (not ignore_path_existence) and (not os.path.isdir(v)):
             raise ValueError('Directory does not exist')
         return v
+
+
+class Version(pydantic.RootModel[str]):
+    """A version string in the format of MAJOR.MINOR.PATCH[-(alpha|beta|rc).N]"""
+
+    root: str = pydantic.Field(
+        ...,
+        pattern=r"^\d+\.\d+\.\d+(-(alpha|beta|rc)\.\d+)?$",
+        examples=["1.2.3", "4.5.6-alpha.78", "7.8.9-beta.10", "11.12.13-rc.14"],
+    )
+
+    def as_tag(self) -> str:
+        """Return the version string as a tag, i.e. vMAJOR.MINOR.PATCH..."""
+        return "v" + self.root
+
+    def as_identifier(self) -> str:
+        """Return the version string as a number, i.e. MAJOR.MINOR.PATCH..."""
+        return self.root
+
+    def _split(
+        self
+    ) -> tuple[int, int, int, Optional[tuple[Literal["alpha", "beta", "rc"],
+                                             int]]]:
+        """Split the version string into MAJOR, MINOR, PATCH, and TAG"""
+        version, tag = self.root.split("-") if "-" in self.root else (
+            self.root, ""
+        )
+        major, minor, patch = map(int, version.split("."))
+        if "-" in self.root:
+            tags = tag.split(".")
+            return major, minor, patch, (tags[0], int(tags[1]))  # type: ignore
+        else:
+            return major, minor, patch, None
+
+    # add comparisons
+    def __lt__(self, other: Version) -> bool:
+        self_major, self_minor, self_patch, self_tag = self._split()
+        other_major, other_minor, other_patch, other_tag = other._split()
+
+        if self_major != other_major:
+            return self_major < other_major
+        if self_minor != other_minor:
+            return self_minor < other_minor
+        if self_patch != other_patch:
+            return self_patch < other_patch
+
+        if self_tag is None:
+            return False
+
+        if other_tag is None:
+            return True
+
+        assert (self_tag is not None) and (other_tag is not None)
+        self_tag_type, self_tag_number = self_tag
+        other_tag_type, other_tag_number = other_tag
+
+        if (self_tag_type == "alpha") and (other_tag_type in ["beta", "rc"]):
+            return True
+        if (self_tag_type == "beta") and (other_tag_type == "rc"):
+            return True
+        if (self_tag_type == "beta") and (other_tag_type == "alpha"):
+            return False
+        if (self_tag_type == "rc") and (other_tag_type in ["alpha", "beta"]):
+            return False
+
+        assert self_tag_type == other_tag_type
+        return self_tag_number < other_tag_number
+
+    def __le__(self, other: Version) -> bool:
+        return (self < other) or (self == other)
+
+    def __gt__(self, other: Version) -> bool:
+        return not (self <= other)
+
+    def __ge__(self, other: Version) -> bool:
+        return not (self < other)
