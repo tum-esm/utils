e6321f05b644a8b6f0e5f3601d2679a7c2dc1da3
Moritz Makowski <moritz.makowski@tum.de>
2024-09-13 21:50:49 +0000

    Add `RandomLabelGenerator` (1)
    add implementation

--- tum_esm_utils/text.py
+++ tum_esm_utils/text.py
@@ -189,3 +189,151 @@ def replace_consecutive_characters(
         while c + c in s:
             s = s.replace(c + c, c)
     return s
+
+
+CONTAINER_ADJECTIVES = set([
+    "admiring", "adoring", "affectionate", "agitated", "amazing", "angry",
+    "awesome", "beautiful", "blissful", "bold", "boring", "brave", "busy",
+    "charming", "clever", "compassionate", "competent", "condescending",
+    "confident", "cool", "cranky", "crazy", "dazzling", "determined",
+    "distracted", "dreamy", "eager", "ecstatic", "elastic", "elated", "elegant",
+    "eloquent", "epic", "exciting", "fervent", "festive", "flamboyant",
+    "focused", "friendly", "frosty", "funny", "gallant", "gifted", "goofy",
+    "gracious", "great", "happy", "hardcore", "heuristic", "hopeful", "hungry",
+    "infallible", "inspiring", "intelligent", "interesting", "jolly", "jovial",
+    "keen", "kind", "laughing", "loving", "lucid", "magical", "modest",
+    "musing", "mystifying", "naughty", "nervous", "nice", "nifty", "nostalgic",
+    "objective", "optimistic", "peaceful", "pedantic", "pensive", "practical",
+    "priceless", "quirky", "quizzical", "recursing", "relaxed", "reverent",
+    "romantic", "sad", "serene", "sharp", "silly", "sleepy", "stoic", "strange",
+    "stupefied", "suspicious", "sweet", "tender", "thirsty", "trusting",
+    "unruffled", "upbeat", "vibrant", "vigilant", "vigorous", "wizardly",
+    "wonderful", "xenodochial", "youthful", "zealous", "zen"
+])
+
+CONTAINER_NAMES = set([
+    "agnesi", "albattani", "allen", "almeida", "antonelli", "archimedes",
+    "ardinghelli", "aryabhata", "austin", "babbage", "banach", "banzai",
+    "bardeen", "bartik", "bassi", "beaver", "bell", "benz", "bhabha",
+    "bhaskara", "black", "blackburn", "blackwell", "bohr", "booth", "borg",
+    "bose", "bouman", "boyd", "brahmagupta", "brattain", "brown", "buck",
+    "burnell", "cannon", "carson", "cartwright", "carver", "cerf",
+    "chandrasekhar", "chaplygin", "chatelet", "chatterjee", "chaum",
+    "chebyshev", "clarke", "cohen", "colden", "cori", "cray", "curie", "curran",
+    "darwin", "davinci", "dewdney", "dhawan", "diffie", "dijkstra", "dirac",
+    "driscoll", "dubinsky", "easley", "edison", "einstein", "elbakyan",
+    "elgamal", "elion", "ellis", "engelbart", "euclid", "euler", "faraday",
+    "feistel", "fermat", "fermi", "feynman", "franklin", "gagarin", "galileo",
+    "galois", "ganguly", "gates", "gauss", "germain", "goldberg", "goldstine",
+    "goldwasser", "golick", "goodall", "gould", "greider", "grothendieck",
+    "haibt", "hamilton", "haslett", "hawking", "heisenberg", "hellman",
+    "hermann", "herschel", "hertz", "heyrovsky", "hodgkin", "hofstadter",
+    "hoover", "hopper", "hugle", "hypatia", "ishizaka", "jackson", "jang",
+    "jemison", "jennings", "jepsen", "johnson", "joliot", "jones", "kalam",
+    "kapitsa", "kare", "keldysh", "keller", "kepler", "khayyam", "khorana",
+    "kilby", "kirch", "knuth", "kowalevski", "lalande", "lamarr", "lamport",
+    "leakey", "leavitt", "lederberg", "lehmann", "lewin", "lichterman",
+    "liskov", "lovelace", "lumiere", "mahavira", "margulis", "matsumoto",
+    "maxwell", "mayer", "mccarthy", "mcclintock", "mclaren", "mclean",
+    "mcnulty", "meitner", "mendel", "mendeleev", "meninsky", "merkle",
+    "mestorf", "mirzakhani", "montalcini", "moore", "morse", "moser", "murdock",
+    "napier", "nash", "neumann", "newton", "nightingale", "nobel", "noether",
+    "northcutt", "noyce", "panini", "pare", "pascal", "pasteur", "payne",
+    "perlman", "pike", "poincare", "poitras", "proskuriakova", "ptolemy",
+    "raman", "ramanujan", "rhodes", "ride", "ritchie", "robinson", "roentgen",
+    "rosalind", "rubin", "saha", "sammet", "sanderson", "satoshi", "shamir",
+    "shannon", "shaw", "shirley", "shockley", "shtern", "sinoussi", "snyder",
+    "solomon", "spence", "stonebraker", "sutherland", "swanson", "swartz",
+    "swirles", "taussig", "tesla", "tharp", "thompson", "torvalds", "tu",
+    "turing", "varahamihira", "vaughan", "villani", "visvesvaraya", "volhard",
+    "wescoff", "wilbur", "wiles", "williams", "williamson", "wilson", "wing",
+    "wozniak", "wright", "wu", "yalow", "yonath", "zhukovsky"
+])
+
+
+class RandomLabelGenerator:
+    """A class to generate random labels that follow the Docker style naming of
+    containers, e.g `admiring-archimedes` or `happy-tesla`.
+    
+    **Usage with tracking duplicates:**
+    
+    ```python
+    generator = RandomLabelGenerator()
+    label = generator.generate()
+    another_label = generator.generate()  # Will not be the same as `label`
+    generator.free(label)  # Free the label to be used again
+    ```
+    
+    **Usage without tracking duplicates:**
+    
+    ```python
+    label = RandomLabelGenerator.generate_fully_random()
+    ```
+    
+    Source for the names and adjectives: https://github.com/moby/moby/blob/master/pkg/namesgenerator/names-generator.go
+    """
+    def __init__(
+        self,
+        occupied_labels: set[str] | list[str] = set(),
+        adjectives: set[str] | list[str] = CONTAINER_ADJECTIVES,
+        names: set[str] | list[str] = CONTAINER_NAMES,
+    ) -> None:
+        """Initialize the label generator."""
+
+        self.occupied_labels = set(occupied_labels)
+        self.adjective_usage_counts: dict[str, int] = {a: 0 for a in adjectives}
+        self.adjectives = set(adjectives)
+        self.names = set(names)
+        for label in occupied_labels:
+            assert re.match(
+                r"^[a-z]+-[a-z]+$", label
+            ), f"Invalid label: {label}"
+            adjective, name = label.split("-")
+            assert adjective in adjectives, f"Invalid adjective: {adjective}"
+            assert name in names, f"Invalid name: {name}"
+            self.adjective_usage_counts[adjective] += 1
+
+    def generate(self) -> str:
+        """Generate a random label that is not already occupied."""
+
+        if len(self.occupied_labels
+              ) == (len(self.adjectives) * len(self.names)):
+            raise RuntimeError("All possible labels are used")
+
+        least_used_adjective, usage_count = min(
+            self.adjective_usage_counts.items(), key=lambda x: x[1]
+        )
+        assert usage_count < len(self.names), "This should not happen"
+
+        used_names = set([
+            x.split("-")[1] for x in self.occupied_labels
+            if x.startswith(least_used_adjective + "-")
+        ])
+        random_name = random.choice(list(self.names - used_names))
+
+        new_label = f"{least_used_adjective}-{random_name}"
+        assert new_label not in self.occupied_labels, "This should not happen"
+        self.occupied_labels.add(new_label)
+
+        self.adjective_usage_counts[least_used_adjective] += 1
+        return new_label
+
+    def free(self, label: str) -> None:
+        """Free a label to be used again."""
+
+        self.occupied_labels.remove(label)
+        self.adjective_usage_counts[label.split("-")[0]] -= 1
+
+    @staticmethod
+    def generate_fully_random(
+        adjectives: set[str] | list[str] = CONTAINER_ADJECTIVES,
+        names: set[str] | list[str] = CONTAINER_NAMES,
+    ) -> str:
+        """Get a random label without tracking duplicates.
+        
+        Use an instance of `RandomLabelGenerator` if you want to avoid
+        duplicates by tracking occupied labels."""
+
+        adjective = random.choice(list(adjectives))
+        name = random.choice(list(names))
+        return f"{adjective}-{name}"
