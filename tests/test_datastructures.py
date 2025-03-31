4b241b4fee7bff277e44a7b98f079661a3f1af03
Moritz Makowski <moritz.makowski@tum.de>
2025-03-31 14:04:52 +0000

    Refactor test file names

--- tests/test_datastructures.py
+++ tests/test_datastructures.py
@@ -88,3 +88,45 @@ def test_ring_list() -> None:
     ring_list.append(23)
     assert ring_list.get() == [18, 19, 20, 21, 22, 23]
     assert ring_list.is_full()
+
+
+@pytest.mark.order(3)
+@pytest.mark.quick
+def test_merge_dicts() -> None:
+    assert tum_esm_utils.datastructures.merge_dicts(
+        old_object={
+            "a": 3,
+            "b": {"c": 50, "e": None},
+            "c": "test",
+        },
+        new_object={
+            "b": {"e": 80},
+        },
+    ) == {
+        "a": 3,
+        "b": {"c": 50, "e": 80},
+        "c": "test",
+    }
+
+    assert tum_esm_utils.datastructures.merge_dicts(
+        old_object={
+            "a": [1, 2, 3],
+        },
+        new_object={
+            "b": {"e": 80},
+        },
+    ) == {
+        "a": [1, 2, 3],
+        "b": {"e": 80},
+    }
+
+    assert tum_esm_utils.datastructures.merge_dicts(
+        old_object={
+            "a": [1, 2, 3],
+        },
+        new_object={
+            "a": {"e": 80},
+        },
+    ) == {
+        "a": {"e": 80},
+    }
