02bd7b128fc90ce49c821fd4a4b2b354b69c3003
Moritz Makowski <moritz.makowski@tum.de>
2025-06-05 16:32:32 +0200

    Add function `download_github_release_asset` (2)
    test

--- tests/test_code.py
+++ tests/test_code.py
@@ -1,3 +1,5 @@
+import os
+import tempfile
 import pytest
 import tum_esm_utils
 
@@ -34,3 +36,23 @@ def test_request_github_file() -> None:
         hostname="gitlab.eudat.eu",
     )
     assert len(c4.replace(" ", "")) > 0, "String c4 is empty"
+
+
+@pytest.mark.order(3)
+@pytest.mark.quick
+def test_download_github_release_asset() -> None:
+    with tempfile.TemporaryDirectory() as tmpdir:
+        for finalname in [None, "anothername.exe"]:
+            for v in ["4.2.2", "4.1.0"]:
+                tum_esm_utils.code.download_github_release_asset(
+                    repository="tum-esm/pyra",
+                    asset_name=f"Pyra.UI_{v}_x64-setup.exe",
+                    final_name=finalname,
+                    dst_dir=tmpdir,
+                )
+                if finalname is None:
+                    finalname = "Pyra.UI_4.2.2_x64-setup.exe"
+                assert os.path.isfile(os.path.join(tmpdir, finalname)), "File not found"
+                assert os.path.getsize(os.path.join(tmpdir, finalname)) > 2 * 1024 * 1024, (
+                    "File size is less than 2MB"
+                )
