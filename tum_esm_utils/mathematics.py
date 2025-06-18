1f236a18ffaf8b2f7189e139c3a1126c71343208
Moritz Makowski <moritz.makowski@tum.de>
2025-06-18 16:42:11 +0200

    Add function `divides_evenly` (1)
    implement

--- tum_esm_utils/mathematics.py
+++ tum_esm_utils/mathematics.py
@@ -9,3 +9,14 @@ def distance_between_angles(angle_1: float, angle_2: float) -> float:
         return min(angle_1 - angle_2, 360 + angle_2 - angle_1)
     else:
         return min(angle_2 - angle_1, 360 + angle_1 - angle_2)
+
+
+def divides_evenly(dividend: float, divisor: float, precision: int = 6) -> bool:
+    """Check if divisor divides dividend evenly.
+
+    Normally this shoudld be done by `dividend % divisor == 0`, but this
+    can lead to floating point errors, i.e. `1 % 0.1 == 0.09999999999999998`.
+    Using `math.fmod` also does not seem to work correctly with floats."""
+
+    multiplier = dividend / divisor
+    return round(multiplier, precision) == round(multiplier)
