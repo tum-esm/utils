d37ed359d592ee8b8d3f94b4b017047feefc732f
Moritz Makowski <moritz.makowski@tum.de>
2025-06-29 16:19:42 +0000

    #28 Improve library import times (2)
    Document explicit import feature

--- tum_esm_utils/__init__.py
+++ tum_esm_utils/__init__.py
@@ -3,7 +3,18 @@ Sensing and Modeling at the Technical University of Munich.
 
 GitHub Repository https://github.com/tum-esm/utils
 Documentation: https://tum-esm-utils.netlify.app
-PyPI: https://pypi.org/project/tum-esm-utils"""
+PyPI: https://pypi.org/project/tum-esm-utils
+
+(Optional) Explicit Imports:
+
+By setting the environment variable `TUM_ESM_UTILS_EXPLICIT_IMPORTS=1`, the
+package disables automatic submodule imports. This means you cannot import the
+whole package and access submodules directly (e.g., `tum_esm_utils.code` will
+not be available after `import tum_esm_utils`). Instead, you must explicitly
+import each submodule, e.g. `from tum_esm_utils import code`.
+
+This reduces the import time of the package by up to 60 times
+"""
 
 import os
 
