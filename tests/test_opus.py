32d910ba883215ef418be6b39344a23ecfc3ef0e
Moritz Makowski <moritz.makowski@tum.de>
2025-07-22 16:18:18 +0000

    A suddpport for old EM27/SUN data in OPUS file reader (2)
    test it on old interferograms

--- tests/test_opus.py
+++ tests/test_opus.py
@@ -2,19 +2,23 @@ import numpy as np
 import pytest
 import tum_esm_utils
 
+
 IFG1 = tum_esm_utils.files.rel_to_abs_path("./data/ifgs/md20220409s0e00a.0198")
 IFG2 = tum_esm_utils.files.rel_to_abs_path("./data/ifgs/md20220409s0e00a.0199")
 IFG3 = tum_esm_utils.files.rel_to_abs_path("./data/ifgs/md20220409s0e00a.0200")
 IFG4 = tum_esm_utils.files.rel_to_abs_path("./data/ifgs/ma20240514s0e00a.0975")
+IFG5 = tum_esm_utils.files.rel_to_abs_path("./data/ifgs/so20170608.ifg.000")
+IFG6 = tum_esm_utils.files.rel_to_abs_path("./data/ifgs/so20170608.ifg.050")
 
 
 @pytest.mark.order(3)
 @pytest.mark.quick
 def test_opus_file_reading() -> None:
     for mode in ["skip", "validate", "read"]:
-        of = tum_esm_utils.opus.OpusFile.read(IFG1, interferogram_mode=mode)  # type: ignore
-        assert (of.interferogram is not None) == (mode == "read")
-        of.model_dump()
+        for f in [IFG1, IFG5, IFG6]:
+            of = tum_esm_utils.opus.OpusFile.read(f, interferogram_mode=mode)  # type: ignore
+            assert (of.interferogram is not None) == (mode == "read")
+            of.model_dump()
 
         # Expect invalid OPUS files to raise a RuntimeError
         try:
