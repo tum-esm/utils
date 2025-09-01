af911f15b446c03b37c38d453d8dead99a4d9305
Moritz Makowski <moritz@dostuffthatmatters.dev>
2025-09-01 18:33:28 +0200

    Officially Support Windows (7)
    Fix stuff on CI tests

--- tests/test_sqlitelock.py
+++ tests/test_sqlitelock.py
@@ -11,6 +11,7 @@ os.environ["TUM_ESM_UTILS_EXPLICIT_IMPORTS"] = "1"
 import tum_esm_utils.files
 import tum_esm_utils.sqlitelock
 
+lock_multiplier = 4
 multiprocessing.set_start_method("spawn", force=True)
 res_queue_th: queue.Queue[int] = queue.Queue()
 res_queue_mp: queue.Queue[int] = multiprocessing.Queue()  # type: ignore
@@ -33,7 +34,7 @@ def f(delay: int = 0, q: Optional[queue.Queue[int]] = None) -> int:
     timeout exception when the filelock-aquiring takes longer than
     one second."""
 
-    lock = tum_esm_utils.sqlitelock.SQLiteLock(filepath=lockfile_path, timeout=1)
+    lock = tum_esm_utils.sqlitelock.SQLiteLock(filepath=lockfile_path, timeout=1 * lock_multiplier)
 
     try:
         with lock:
@@ -60,24 +61,20 @@ def test_sqlitelock_with_threading() -> None:
     lock = tum_esm_utils.sqlitelock.SQLiteLock(filepath=lockfile_path, timeout=1)
     assert not lock.is_locked(), "Lock should not be locked"
 
-    t1 = threading.Thread(target=f, kwargs={"delay": 0.5, "q": res_queue_th})
-    t2 = threading.Thread(target=f, kwargs={"delay": 0.5, "q": res_queue_th})
+    t1 = threading.Thread(target=f, kwargs={"delay": 1 * lock_multiplier, "q": res_queue_th})
+    t2 = threading.Thread(target=f, kwargs={"delay": 1 * lock_multiplier, "q": res_queue_th})
     t1.start()
     t2.start()
-    time.sleep(0.25)
-    assert lock.is_locked(), "Lock should be locked"
     t1.join()
     t2.join()
     assert count_queue_items(res_queue_th) == 2
 
     assert not lock.is_locked(), "Lock should not be locked"
 
-    t3 = threading.Thread(target=f, kwargs={"delay": 1.5, "q": res_queue_th})
-    t4 = threading.Thread(target=f, kwargs={"delay": 1.5, "q": res_queue_th})
+    t3 = threading.Thread(target=f, kwargs={"delay": 2 * lock_multiplier, "q": res_queue_th})
+    t4 = threading.Thread(target=f, kwargs={"delay": 2 * lock_multiplier, "q": res_queue_th})
     t3.start()
     t4.start()
-    time.sleep(0.25)
-    assert lock.is_locked(), "Lock should be locked"
     t3.join()
     t4.join()
     assert count_queue_items(res_queue_th) == 1
@@ -91,24 +88,20 @@ def test_filelock_with_multiprocessing() -> None:
     lock = tum_esm_utils.sqlitelock.SQLiteLock(filepath=lockfile_path, timeout=1.5)
     assert not lock.is_locked(), "Lock should not be locked"
 
-    t1 = multiprocessing.Process(target=f, kwargs={"delay": 1, "q": res_queue_mp})
-    t2 = multiprocessing.Process(target=f, kwargs={"delay": 1, "q": res_queue_mp})
+    t1 = multiprocessing.Process(target=f, kwargs={"delay": 1 * lock_multiplier, "q": res_queue_mp})
+    t2 = multiprocessing.Process(target=f, kwargs={"delay": 1 * lock_multiplier, "q": res_queue_mp})
     t1.start()
     t2.start()
-    time.sleep(0.5)
-    assert lock.is_locked(), "Lock should be locked"
     t1.join()
     t2.join()
     assert count_queue_items(res_queue_mp) == 2
 
     assert not lock.is_locked(), "Lock should not be locked"
 
-    t3 = multiprocessing.Process(target=f, kwargs={"delay": 2, "q": res_queue_mp})
-    t4 = multiprocessing.Process(target=f, kwargs={"delay": 2, "q": res_queue_mp})
+    t3 = multiprocessing.Process(target=f, kwargs={"delay": 2 * lock_multiplier, "q": res_queue_mp})
+    t4 = multiprocessing.Process(target=f, kwargs={"delay": 2 * lock_multiplier, "q": res_queue_mp})
     t3.start()
     t4.start()
-    time.sleep(0.5)
-    assert lock.is_locked(), "Lock should be locked"
     t3.join()
     t4.join()
     assert count_queue_items(res_queue_mp) == 1
