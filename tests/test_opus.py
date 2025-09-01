b84884bebb93367e40cf4275704e86c16e3ec1ce
Moritz Makowski <moritz@dostuffthatmatters.dev>
2025-09-01 22:58:47 +0200

    Officially Support Windows (20)
    Debug

--- tests/test_opus.py
+++ tests/test_opus.py
@@ -1,3 +1,4 @@
+import os
 import numpy as np
 import pytest
 import tum_esm_utils.opus
@@ -14,6 +15,7 @@ IFG6 = tum_esm_utils.files.rel_to_abs_path("./data/ifgs/so20170608.ifg.050")
 
 @pytest.mark.order(3)
 @pytest.mark.quick
+@pytest.mark.skipif(os.name != "posix", reason="Not supported on Windows")
 def test_opus_file_reading() -> None:
     for mode in ["skip", "validate", "read"]:
         for f in [IFG1, IFG5, IFG6]:
